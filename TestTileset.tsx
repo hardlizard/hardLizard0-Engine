488352565eaabcf2fe7231fb3c57f806be4baeed
John Emory <jfemory@gmail.com>
2019-06-06 01:13:04 -0500

    added worldmap json loading

--- TestTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="TestTileset" tilewidth="16" tileheight="16" tilecount="270" columns="27">
- <image source="DungeonTileset.png" width="432" height="160"/>
-</tileset>
